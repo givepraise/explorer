2f9f7accbc75c9c6a9e7ad7247df943299a673c3
kristoferlund <kristofer@fmckl.se>
2023-06-07 13:17:20 +0200

    Open link in new window

--- src/app/components/community-card.tsx
+++ src/app/components/community-card.tsx
@@ -16,7 +16,7 @@ export default function CommunityCard({
       key={community._id.toString()}
       className="inline-block p-8 text-base font-normal prose transition duration-200 ease-in-out rounded-lg cursor-pointer bg-slate-100 hover:-translate-y-1 hover:scale-105 drop-shadow-lg"
       onClick={() => {
-        window.location.href = `https://${community.hostname}`;
+        window.open(`https://${community.hostname}`);
       }}
     >
       {children}
